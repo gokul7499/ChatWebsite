6fcf970db8b025f213453c57d9b68f441a338b9d
gudaghers <gudaghers@gmail.com>
2025-02-01 11:06:43 +0530

    no changs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatBox from "./pages/ChatBox/ChatBox";
 import SignUp from "./pages/SignUp/SignUp";
 import LogIn from "./pages/LogIn/LogIn";
-import { auth } from "./firebase";
 import Home from "./pages/Home/Home";
-import { ThreeCircles } from "react-loader-spinner";
 import ContactUs from "./pages/ContactUs/ContactUs";
+import { auth } from "./firebase";
+import { ThreeCircles } from "react-loader-spinner";
 
 function App() {
   const [userName, setUserName] = useState("");
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
     }, 2000);
+
     auth.onAuthStateChanged((user) => {
-      // console.log(user);
       if (user) {
         setUserName(user.displayName);
       } else {
@@ -35,22 +36,18 @@ function App() {
             height="50"
             width="50"
             color="#046cf1"
-            wrapperStyle={{}}
-            wrapperClass=""
             visible={true}
             ariaLabel="three-circles-rotating"
-            outerCircleColor=""
-            innerCircleColor=""
-            middleCircleColor=""
           />
         </div>
       ) : (
         <Router>
           <Routes>
-            <Route path="/home" element={<Home />} />
             <Route path="/" element={<SignUp />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/login" element={<LogIn />} />
             <Route path="/chatbox" element={<ChatBox name={userName} />} />
+            <Route path="/ChatWebsite" element={<ChatBox name={userName} />} />
             <Route path="/contactus" element={<ContactUs />} />
           </Routes>
         </Router>
